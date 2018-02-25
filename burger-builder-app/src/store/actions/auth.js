66b38e008819ff29c6c7d582680693a4a8d669be
lxieyang <lxieyang@umich.edu>
2018-02-25 00:33:40 -0500

    add saga

--- burger-builder-app/src/store/actions/auth.js
+++ burger-builder-app/src/store/actions/auth.js
@@ -23,11 +23,14 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
-  localStorage.removeItem('token');
-  localStorage.removeItem('expirationDate');
-  localStorage.removeItem('userId');
+  // localStorage.removeItem('token');
+  // localStorage.removeItem('expirationDate');
+  // localStorage.removeItem('userId');
+  // return {
+  //   type: actionTypes.AUTH_LOGOUT
+  // }
   return {
-    type: actionTypes.AUTH_LOGOUT
+    type: actionTypes.AUTH_INITIATE_LOGOUT
   }
 }
 
